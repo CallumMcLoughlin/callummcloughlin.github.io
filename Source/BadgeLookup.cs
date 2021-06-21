1882d7c608608b73c36b4e232096293d4e4900b5
CallumMcLoughlin <59436970+CallumMcLoughlin@users.noreply.github.com>
2021-06-21 23:39:56 +1200

    Added VueCertbot project to portfolio and badges. Might need to refactor to make them look slightly better :)

---
+++ Source/BadgeLookup.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Showcase.Shared
+{
+    public class BadgeLookup
+    {
+        public static Dictionary<string, string> badgeColors = new Dictionary<string, string>()
+        {
+            { "Java", "#ba8b00" },
+            { "C#", "#3cd400" },
+            { "ASP.NET", "#3cd400" },
+            { "WASM", "#3cd400" },
+            { "C", "#696969" },
+            { "HTML", "#d11313" },
+            { "CSS", "#9513d1" },
+            { "Javascript", "#ede624" },
+            { "Vue", "#62cc21" },
+            { "Python", "#ba8b00" },
+            { "Docker", "#4b613d" },
+            { "Shell", "#85ff38" },
+        };
+    }
+}
